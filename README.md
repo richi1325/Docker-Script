83a2d147dffc038b36d6105b896f205f12932024
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2023-06-19 16:38:10 -0600

    Fix: docker run command in .bash -> README.md

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ This code constains a script for install docker in ubuntu
 ```
 sudo chmod +x docker.bash
 ./docker.bash
+sudo docker run -d --name Jupyter --mount type=bind,source="/mnt/c/Users/...",target=/work -p 8888:8888 richi1325/spylon:1.0.0
 ```
--- docker.bash
+++ docker.bash
@@ -11,10 +11,5 @@ echo \
 sudo apt-get -y update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo service docker start
-container_id=$(sudo docker run -d --name Jupyter --mount type=bind,source="/mnt/c/Users/...",target=/work -p 8888:8888 richi1325/spylon:1.0.0)
-if [ -n "$container_id" ]; then
-  echo "sudo service docker start" >> $HOME/.bashrc
-  echo "sudo docker start Jupyter" >> $HOME/.bashrc
-else
-  echo "No se pudo crear el contenedor"
-fi
+echo "sudo service docker start" >> $HOME/.bashrc
+echo "sudo docker start Jupyter" >> $HOME/.bashrc
