3cc0dc97b84f80f7cf5f4cf8178d5538804a5089
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2023-06-19 08:51:25 -0600

    first commit

---
+++ docker.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run -d --name Jupyter --mount type=bind,source="/mnt/c/Users/..." ,target=/work -p 8888:8888 richi1325/spylon:1.0.0
+echo "sudo service docker start" >> $HOME/.bashrc
+echo "sudo docker start Jupyter" >> $HOME/.bashrc
